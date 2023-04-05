9b17672c7705f8d384da8709ee3dc4148fef4f69
Sabri Derbala <sabri.derbala@gmail.com>
2023-04-05 06:20:14 +0200

    modif front

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,4 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "pages#home"
-  get "about", to: "pages#about"
-  get "contact", to: "pages#contact"
-  get "projects", to: "pages#projects"
 end
